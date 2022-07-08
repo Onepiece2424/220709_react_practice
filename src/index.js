5621a59997df0b5eec00d553de14eec30f56bc43
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-07-09 08:23:54 +0900

    querySelectorAllによるElementの取得 querySelectorAllを使用することで、一致するElementを全て取得できる

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 //getElementedByIdを使う
-const title1 = document.getElementById("title");
-console.log("title1");
+// const title1 = document.getElementById("title");
+// console.log("title1");
 // //<h1 id="title">Hello World!!</h1>の取得
 
 // //querySelectorを使う
-const title2 = document.querySelector("#title");
-console.log("title2");
+// const title2 = document.querySelector("#title");
+// console.log("title2");
 //<h1 id="title">Hello World!!</h1>の取得
 
 //Class名でElementを取得
 //getElementsByClassNameを使う
-const containers = document.getElementsByClassName("container");
-console.log(containers);
+// const containers = document.getElementsByClassName("container");
+// console.log(containers);
 
 //querySelectorによるElementの取得
 //querySelectorを使う
-const container = document.querySelector(".container");
-console.log(container);
+// const container = document.querySelector(".container");
+// console.log(container);
+
+//querySelectorAllによるElementの取得
+//querySelectorAllを使う
+const containers = document.querySelectorAll(".containers");
+console.log(containers);
