5ed96b15322a2569d0ee62146e376ea5cefce865
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-07-09 08:18:38 +0900

    querySelectorによるElementの取得

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 //getElementedByIdを使う
 const title1 = document.getElementById("title");
 console.log("title1");
-//<h1 id="title">Hello World!!</h1>の取得
+// //<h1 id="title">Hello World!!</h1>の取得
 
-//querySelectorを使う
+// //querySelectorを使う
 const title2 = document.querySelector("#title");
 console.log("title2");
 //<h1 id="title">Hello World!!</h1>の取得
@@ -12,3 +12,8 @@ console.log("title2");
 //getElementsByClassNameを使う
 const containers = document.getElementsByClassName("container");
 console.log(containers);
+
+//querySelectorによるElementの取得
+//querySelectorを使う
+const container = document.querySelector(".container");
+console.log(container);
